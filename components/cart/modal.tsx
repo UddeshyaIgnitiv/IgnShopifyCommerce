6de4dbdf38131fc743da02fe66cbc9a55cb7efef
ranjeetAtignitiv <ranjeet.d@ignitiv.com>
2025-08-19 18:15:13 +0530

    Updated hooks

--- components/cart/modal.tsx
+++ components/cart/modal.tsx
@@ -7,6 +7,7 @@ import LoadingDots from 'components/loading-dots';
 import Price from 'components/price';
 import { DEFAULT_OPTION } from 'lib/constants';
 import { createUrl } from 'lib/utils';
+import { useUserRole } from 'lib/utils/useUserRole';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Fragment, useEffect, useRef, useState } from 'react';
@@ -20,42 +21,6 @@ import OpenCart from './open-cart';
 type MerchandiseSearchParams = {
   [key: string]: string;
 };
-
-function useUserRole() {
-  const [role, setRole] = useState<string | null>(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchRole() {
-      //console.log('[useUserRole] Fetching user role...');
-      try {
-        const res = await fetch('/api/users');
-        if (!res.ok) throw new Error('Failed to fetch user role');
-        const data = await res.json();
-        //console.log("data", data);
-        //console.log('[useUserRole] Role fetched:', data.role);
-
-        // Sanitize role: remove extra quotes if present
-        let cleanRole = data.role;
-        if (typeof cleanRole === 'string') {
-          cleanRole = cleanRole.replace(/^"(.*)"$/, '$1');
-        }
-
-        //console.log('[useUserRole] Role fetched:', cleanRole);
-
-        setRole(cleanRole  || 'purchaser'); // default fallback
-      } catch (error) {
-        console.error('Error fetching role:', error);
-        setRole('purchaser');
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchRole();
-  }, []);
-
-  return { role, loading };
-}
 
 export default function CartModal() {
   const { cart, updateCartItem } = useCart();
