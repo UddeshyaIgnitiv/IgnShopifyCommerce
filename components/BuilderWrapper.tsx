c46f8748cd5b960e5cc0ca1e097ff422acb3b6fe
Souvik-13254 <souvik.p@ignitiv.com>
2025-08-26 17:17:59 +0530

    error fix

--- components/BuilderWrapper.tsx
+++ components/BuilderWrapper.tsx
@@ -2,14 +2,23 @@
 'use client';
 
 import { BuilderComponent, builder } from '@builder.io/react';
+import { useEffect, useState } from 'react';
 
 // Optional: only call init here if not done elsewhere
 builder.init(process.env.NEXT_PUBLIC_BUILDER_IO_API_KEY!);
 
-export default function BuilderWrapper({ content }: { content: any }) {
-  if (!content) {
-    return <div>Page not found</div>;
-  }
+//old code
+// export default function BuilderWrapper({ content }: { content: any }) {
+//   if (!content) {
+//     return <div>Page not found</div>;
+//   }
 
+//   return <BuilderComponent model="page" content={content} />;
+// }
+
+export default function BuilderClient({ content }: { content: any }) {
+  const [isMounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  if (!content || !isMounted) return null;
   return <BuilderComponent model="page" content={content} />;
 }
