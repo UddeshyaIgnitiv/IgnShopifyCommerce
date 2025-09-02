b778100399b3bbc7c89af457a11776222d32d3c4
Souvik-13254 <souvik.p@ignitiv.com>
2025-09-02 19:30:57 +0530

    Plp Pagination

--- app/search/[collection]/page.tsx
+++ app/search/[collection]/page.tsx
@@ -3,7 +3,6 @@ import { Metadata } from 'next';
 import { cookies } from 'next/headers';
 import { notFound } from 'next/navigation';
 
-import Grid from 'components/grid';
 import ProductGridItems from 'components/layout/product-grid-items';
 import { defaultSort, sorting } from 'lib/constants';
 
@@ -47,9 +46,7 @@ export default async function CategoryPage(props: {
       {products.length === 0 ? (
         <p className="py-3 text-lg">{`No products found in this collection`}</p>
       ) : (
-        <Grid className="grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
           <ProductGridItems products={products} />
-        </Grid>
       )}
     </section>
   );
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,4 +1,3 @@
-import Grid from 'components/grid';
 import ProductGridItems from 'components/layout/product-grid-items';
 import { defaultSort, sorting } from 'lib/constants';
 import { getProducts } from 'lib/shopify';
@@ -42,9 +41,7 @@ export default async function SearchPage(props: {
         </p>
       ) : null}
       {products.length > 0 ? (
-        <Grid className="grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
           <ProductGridItems products={products} />
-        </Grid>
       ) : null}
     </>
   );
